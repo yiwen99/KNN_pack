a2c86f365cd05da0e8b00b86e30153339c691c61
Yiwen Chen <chenyiwen_99@126.com>
2021-12-03 11:22:33 -0500

    added tests

--- tests/testthat/test-knn1_func.R
+++ tests/testthat/test-knn1_func.R
@@ -1,3 +1,30 @@
-test_that("multiplication works", {
+test_that("knn1 works", {
+  library(class)
   expect_equal(2 * 2, 4)
+  train <- matrix(c(9,2,2,3,9,0,1,9,9,8,2,3,6,9,4,2,3,6),ncol=2)
+  cl <- as.factor(c("a","b","b","b","c","c","c","c","c"))
+  test1 <- c(2,3.5)
+
+  #check how knn1_func is working on classifying one test case
+  my_knn1_output <- knn1_func(train, test1, cl)
+  real_knn1_output <- knn1(train, test1, cl)
+  expect_equal(my_knn1_output, real_knn1_output)
+
+  #check on randomness of knn1_func when there are two observations
+  #in the training set that are both the closest
+  set.seed(1)
+  train_new <- train
+  train_new[8,] <- c(2,2)
+  random_1 <- knn1_func(train_new, c(2,2),cl)
+  expected_1 <- factor(c("b"),levels=c("a","b","c"))
+  expect_equal(random_1, expected_1)
+
+  set.seed(0)
+  random_2 <- knn1_func(train_new, c(2,2),cl)
+  expected_2 <- factor(c("c"),levels=c("a","b","c"))
+  expect_equal(random_2, expected_2)
+
+  #test on error messages
+
+
 })
--- tests/testthat/test-knn_func.R
+++ tests/testthat/test-knn_func.R
@@ -1,3 +1,34 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("knn works", {
+  #expect_equal(2 * 2, 4)
+  train <- matrix(c(9,2,2,3,9,0,1,9,9,8,2,3,6,9,4,2,3,6),ncol=2)
+  cl <- as.factor(c("a","b","b","b","c","c","c","c","c"))
+  test1 <- c(2,3.5)
+  my_knn_output <- knn_func(train,test1, cl, 3, prob=TRUE)
+  library(class)
+  real_knn_output <- knn(train=train, test=test1, cl=cl, k=3, prob=TRUE)
+  # check how knn_func is working on classifying 1 test case,
+  #with prob parameter set to be TRUE
+  expect_equal(my_knn_output, real_knn_output)
+
+  #check how knn_func is working on classifying 1 test case,
+  #with prob parameter set to be FALSE
+  my_knn_output1 <- knn_func(train,test1, cl, 3, prob=FALSE)
+  real_knn_output1 <- knn(train=train, test=test1, cl=cl, k=3, prob=FALSE)
+  expect_equal(my_knn_output1, real_knn_output1)
+
+  test <- matrix(c(2,9,3.5,7.5),ncol=2)
+  my_knn_output2 <- knn_func(train, test, cl, k=3, prob=TRUE)
+  real_knn_output2 <- knn(train=train, test=test, k=3, cl=cl, prob=TRUE)
+  #check how knn_func is working on classifying multiple test cases,
+  #with prob parameter set to be TRUE
+  expect_equal(my_knn_output2, real_knn_output2)
+
+  #check how knn_func is working on classifying multiple test cases,
+  #with prob parameter set to be FALSE
+  my_knn_output3 <- knn_func(train, test, cl, k=3, prob=FALSE)
+  real_knn_output3 <- knn(train, test, cl, k=3, prob=FALSE)
+  expect_equal(my_knn_output3, real_knn_output3)
+
+  ##check on error messages
+
 })
